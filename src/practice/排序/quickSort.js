4d8c2b9115526418ca7a5fa909e5799c899a3e7c
blade-demon <xuziwei89@gmail.com>
2022-11-30 08:30:21 +0800

    优化快排

--- src/practice/排序/quickSort.js
+++ src/practice/排序/quickSort.js
@@ -1,5 +1,14 @@
-// https://mp.weixin.qq.com/s?__biz=Mzg2NzA4MTkxNQ==&mid=2247485191&idx=1&sn=45a43bd77495566db53b419ae82136f5&source=41#wechat_redirect
+// 平均时间复杂度O(NlogN)，每次分割的时候，主元左边的元素个数都为0，而右边都为n-1个。这个时候，就需要分割n次了。而每次分割整理的时间复杂度为O(n)，所以最坏的时间复杂度为O(n2)。
 // 快排：双向扫描
+
+// 随机选取主元，降低平均时间复杂度
+function randomPartion(arr, left, right) {
+  let i = Math.floor(Math.random() * (right - left + 1));
+  [arr[i], arr[left]] = [arr[left], arr[i]];
+
+  return partion(arr, left, right);
+}
+
 function partion(arr, left, right) {
   let i = left + 1;
   let j = right;
@@ -28,6 +37,7 @@ function quickSort(arr = [], left = 0, right = arr.length - 1) {
     quickSort(arr, left, center - 1);
     quickSort(arr, center + 1, right);
   }
+  return arr;
 }
 
-console.log(quickSort([42, 345, 0, 23, 78, 23, 98]));
+console.log(quickSort([42, 345, 0, 2, 34, -10, 23, 78, 23, 98]));
